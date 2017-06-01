172d79c53f20f25885c725c380415212d92cda13
trevormac <trevor.c.macgregor@gmail.com>
2017-06-01 15:44:02 -0400

    Initial Commit

---
+++ SQLite_Contact_List/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SQLite_Contact_List
+//
+//  Created by Trevor MacGregor on 2017-06-01.
+//  Copyright © 2017 TeevoCo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
